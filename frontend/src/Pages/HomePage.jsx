04c284a1ae0c3bea189df76cf4ac8b5591d73d3f
John Rocha <xjohnfitcodes@gmail.com>
2024-11-04 15:47:17 -0500

    HomePage.jsx updated

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -56,7 +56,7 @@ const NoMoreProfiles = () => (
 	<div className='flex flex-col items-center justify-center h-full text-center p-8'>
 		<Frown className='text-pink-400 mb-6' size={80} />
 		<h2 className='text-3xl font-bold text-gray-800 mb-4'>No more profiles around</h2>
-		<p className='text-xl text-gray-600 mb-6'>Bro are you OK? Maybe it&apos;s time to touch some grass.</p>
+		<p className='text-xl text-gray-600 mb-6'>Check back later homie</p>
 	</div>
 );
 
